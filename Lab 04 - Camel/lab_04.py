ba20ffa6a26dd9be45d27dd4011f8f1a43c03a3b
iofag <iofagbohun@gmail.com>
2023-02-09 12:15:56 -0500

    LAB 4

--- Lab 04 - Camel/lab_04.py
+++ Lab 04 - Camel/lab_04.py
@@ -6,17 +6,20 @@ def main():
     print("You have stolen a camel to make your way across the great Mobi desert.")
     print("The natives want their camel back and are chasing you down! Survive your")
     print("desert trek and out run the natives.")
-done = False
+    get_input()
 
-while not done:
-    print("A. Drink from your canteen.")
-    print("B. Ahead moderate speed.")
-    print("C. Ahead full speed.")
-    print("D. Stop for the night.")
-    print("E. Status check.")
-    print("Q. Quit.")
-    user_choice = input("what is your choice? ")
-    user_choice.upper()
+def get_input():
+    done = False
+    while not done:
+        print("A. Drink from your canteen.")
+        print("B. Ahead moderate speed.")
+        print("C. Ahead full speed.")
+        print("D. Stop for the night.")
+        print("E. Status check.")
+        print("Q. Quit.")
+        user_choice = input("what is your choice? ")
+        if user_choice.upper() == 'Q':
+            print("thanks for playing")
+            done = True
 
-
-    main()
+main()
