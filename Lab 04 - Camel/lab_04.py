30911cafce9e5150f36d131b84b09b90f051e321
iofag <iofagbohun@gmail.com>
2023-02-10 10:29:27 -0500

    LAB 4 edit

--- Lab 04 - Camel/lab_04.py
+++ Lab 04 - Camel/lab_04.py
@@ -1,4 +1,12 @@
 import arcade
+import random
+
+# Variables
+miles_traveled = 0
+thirst = 0
+camel_tiredness = 0
+native_traveled = -20
+canteen = 3
 
 
 def main():
@@ -8,9 +16,11 @@ def main():
     print("desert trek and out run the natives.")
     get_input()
 
-def get_input():
+
+def get_input(native_traveled=-20, camel_tiredness=0):
     done = False
     while not done:
+        natives_behind = miles_traveled - native_traveled
         print("A. Drink from your canteen.")
         print("B. Ahead moderate speed.")
         print("C. Ahead full speed.")
@@ -21,6 +31,17 @@ def get_input():
         if user_choice.upper() == 'Q':
             print("thanks for playing")
             done = True
+        # status check
+        elif user_choice.upper() == "e":
+            print("Miles traveled", miles_traveled)
+            print("Drinks in canteen", canteen)
+            print("your camel has", camel_tiredness, "amount of fatigue")
+            print("The natives are", natives_behind, "miles behind you")
+        # stop for the night
+        elif user_choice.upper() == "d":
+            camel_tiredness *= 0
+            print("camel is happy")
+            native_traveled += random.randrange(7, 15)
 
 
 main()
