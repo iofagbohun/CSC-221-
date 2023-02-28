9dba8d2da09c2195ecb86cd45c708d516abb287c
iofag <iofagbohun@gmail.com>
2023-02-27 22:53:03 -0500

    Lab 6

---
+++ Lab 06 - Text Adventure/lab_06.py
@@ -0,0 +1,8 @@
+class Room:
+    """This is a class"""
+    def __int__(self):
+        self.description = " "
+        self.north = 0
+        self.east = 0
+        self.south = 0
+        self.west = 0
