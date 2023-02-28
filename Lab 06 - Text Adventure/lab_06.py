0ae8c8440a3b1a096d42e29667d7b8d71b1e2da7
iofag <iofagbohun@gmail.com>
2023-02-27 23:23:39 -0500

    Lab 6

--- Lab 06 - Text Adventure/lab_06.py
+++ Lab 06 - Text Adventure/lab_06.py
@@ -1,8 +1,12 @@
 class Room:
     """This is a class"""
-    def __int__(self):
-        self.description = " "
-        self.north = 0
-        self.east = 0
-        self.south = 0
-        self.west = 0
+    def __int__(self, description, north, east, south, west):
+        self.description = description
+        self.north = north
+        self.east = east
+        self.south = south
+        self.west = west
+def main():
+    """This is the main function"""
+    # Creating empty list
+    room_list = []
