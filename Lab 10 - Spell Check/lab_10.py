b1679d4ecf0079a08beaffdad811666415306a1e
iofag <iofagbohun@gmail.com>
2023-04-11 12:48:55 -0400

    Lab 10

---
+++ Lab 10 - Spell Check/lab_10.py
@@ -0,0 +1,31 @@
+import re
+
+# This function takes in a line of text and returns
+# a list of words in the line.
+def split_line(line):
+    return re.findall('[A-Za-z]+(?:\'[A-Za-z]+)?',line)
+
+def main():
+    """ Read in lines from a file """
+
+    # Open the file for reading, and store a pointer to it in the new
+    # variable "file"
+    my_file = open("dictionary.txt")
+
+    # Create an empty list to store our names
+    dictionary_list = []
+
+    # Loop through each line in the file like a list
+    for line in my_file:
+        # Remove any line feed, carriage returns or spaces at the end of the line
+        line = line.strip()
+
+        # Add the name to the list
+        dictionary_list.append(line)
+
+    my_file.close()
+
+    print( "There were", len(dictionary_list), "names in the file.")
+
+
+main()
