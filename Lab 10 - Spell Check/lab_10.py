228bb2f5c921f08f697ddce939512b3a33cebd54
iofag <iofagbohun@gmail.com>
2023-04-12 12:13:53 -0400

    LAB 10 - in progress 2

--- Lab 10 - Spell Check/lab_10.py
+++ Lab 10 - Spell Check/lab_10.py
@@ -6,16 +6,20 @@ import re
 
 
 def split_line(line):
-    split = re.findall('[A-Za-z]+(?:\'\"[A-Za-z]+)?', line)
+    split = re.findall('[A-Za-z]+(?:\'[A-Za-z]+)?', line)
     return split
 
 
 # Opens the dictionary text file and adds each line to an array, then closes the file
 dictionary = open("dictionary.txt")
 dictionary_list = []
+lower_bound = 0
+upper_bound = len(dictionary_list)-1
+found = False
 for item in dictionary:
-    dictionary_list.append(split_line(item))
-print(dictionary_list)
+    item = item.strip() # strips off carriage returns
+    dictionary_list.append(item)
+#print(dictionary_list)
 dictionary.close()
 
 print("---Linear Search---")
@@ -24,10 +28,35 @@ print("---Linear Search---")
 chapter_1 = open("AliceInWonderland200.txt")
 
 # Breaks down the text by line
+line_num = 0
 for each_line in chapter_1:
+    line_num += 1
     # Breaks down each line to a single word
     word_list = split_line(each_line)
     # Checks each word against the dictionary array
     for each_word in word_list:
-        current_word_list = 0
+        i = 0 # initialize to zero
+        # check dictionary list comparing each word to the curry word being search for
+        while i < len(dictionary_list) and dictionary_list[i] != each_word.upper():
+            i += 1
+
+        if i==len(dictionary_list):
+            print(each_word,'possibly misspelled at line',line_num)
+
+# Closes the first chapter file
+chapter_1.close()
+
+print("--- Binary Search ---")
+
+while lower_bound <= upper_bound and not found:
+    middle_position = (lower_bound + upper_bound) // 2
+
+    # figure out if we need to move up or down
+    if dictionary_list[middle_position] < each_word.upper:
+        lower_bound = middle_position + 1
+    elif dictionary_list [middle_position] > each_word.upper:
+        upper_bound = middle_position -1
+    else:
+        found = True
+
 
