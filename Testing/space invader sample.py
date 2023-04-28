0c1f3dda85d268fd9e45e90b8bd5a8b22b64c804
iofag <iofagbohun@gmail.com>
2023-04-28 15:13:33 -0400

    lab 12- windown opens

--- Testing/space invader sample.py
+++ Testing/space invader sample.py
@@ -41,30 +41,7 @@ class Alien2_ship2(arcade.Sprite):
             self.center_y = SCREEN_HEIGHT
 
 
-class InstructionView(arcade.View):
-    """ View to show instructions """
 
-    def on_show_view(self):
-        """ This is run once when we switch to this view """
-        arcade.set_background_color(arcade.csscolor.DARK_SLATE_BLUE)
-
-        # Reset the viewport, necessary if we have a scrolling game and we need
-        # to reset the viewport back to the start so we can see what we draw.
-        arcade.set_viewport(0, self.window.width, 0, self.window.height)
-
-    def on_draw(self):
-        """ Draw this view """
-        self.clear()
-        arcade.draw_text("Instructions Screen", self.window.width / 2, self.window.height / 2,
-                         arcade.color.WHITE, font_size=50, anchor_x="center")
-        arcade.draw_text("Click to advance", self.window.width / 2, self.window.height / 2-75,
-                         arcade.color.WHITE, font_size=20, anchor_x="center")
-
-    def on_mouse_press(self, _x, _y, _button, _modifiers):
-        """ If the user presses the mouse button, start the game. """
-        MyGame()
-        MyGame.setup()
-        self.window.show_view()
 
 
 class MyGame(arcade.Window):
