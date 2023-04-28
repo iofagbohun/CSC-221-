3457990ca5144a5d12bf378d5ec042fce4bd5e00
iofag <iofagbohun@gmail.com>
2023-04-28 13:00:43 -0400

    lab 12- need to add instruction screen

--- Testing/space invader sample.py
+++ Testing/space invader sample.py
@@ -62,9 +62,9 @@ class InstructionView(arcade.View):
 
     def on_mouse_press(self, _x, _y, _button, _modifiers):
         """ If the user presses the mouse button, start the game. """
-        game_view = GameView()
-        game_view.setup()
-        self.window.show_view(game_view)
+        game_view = MyGame()
+        MyGame.setup()
+        self.window.show_view()
 
 
 class MyGame(arcade.Window):
